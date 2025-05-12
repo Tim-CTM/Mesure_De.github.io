a619187a2f8583a2314c3c53f86f9499daa1c873
Tim <mouatim.ctm@gmail.com>
2025-05-12 18:55:44 +0300

    Ajout page Apercu et modif code php + commentaire

--- Projet Mesure/database.php
+++ Projet Mesure/database.php
@@ -1,6 +1,5 @@
 <?php
-
-$servername = "127.0.0.1";
+$servername = "192.168.17.10";
 $username = "root";
 $password = "lamp";
 $dbname = "Mesure_De";
@@ -14,23 +13,40 @@ if ($conn->connect_error)
 
 $sql = "SELECT Timestamp, Valeur_Mesure FROM Donnee_Mesurer";
 
+//Exécution de la requête SQL
 $result = $conn->query($sql);
 
-$data = array();
+//Tableau pour stocker les totaux par jour
+$sumPerDays = array();
 
+// Vérification s'il y a des résultats
 if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc())  //Tant qu'il y à des info on récupère les colonnes de la BDD
+    while ($row = $result->fetch_assoc())
     {
-        $data[] = array(
-            "timestamp" => $row["Timestamp"],
-            "valeur" => $row["Valeur_Mesure"]
-        );
+        $timestamp = $row["Timestamp"];
+        $valeur = floatval($row["Valeur_Mesure"]);
+        $datePart = substr($timestamp, 0, 10); //Les 10 premiers caractère donc la date ex : 2025-05-12
+
+        // Addition des valeurs pour chaque jour de la BDD
+        if (isset($sumPerDays[$datePart])) {
+            $sumPerDays[$datePart] += $valeur;
+        } else {
+            $sumPerDays[$datePart] = $valeur;
+        }
     }
 }
 
+//Tableau de sortie au format JSON
+$output = array();
+foreach ($sumPerDays as $date => $sum) {
+    $output[] = array(
+        "timestamp" => $date, //Nouveau tableau associatif
+        "valeur" => $sum //La somme des mesures pour cette date
+    );
+}
+
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($output);
 
 $conn->close();
-
 ?>
--- Projet Mesure/export-csv.php
+++ Projet Mesure/export-csv.php
@@ -1,33 +1,36 @@
 <?php
-$servername = "192.168.17.10";
-$username = "root";
-$password = "lamp";
-$dbname = "Mesure_De";
+$serveur = "192.168.17.10";
+$utilisateur = "root";
+$mot_de_passe = "lamp";
+$nom_base = "Mesure_De";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$connexion = new mysqli($serveur, $utilisateur, $mot_de_passe, $nom_base);
 
-if ($conn->connect_error) {
-    die("La connexion a échoué : " . $conn->connect_error);
+if ($connexion->connect_error) {
+    die("Impossible de se connecter : " . $connexion->connect_error);
 }
 
-$sql = "SELECT Timestamp, Valeur_Mesure FROM Donnee_Mesurer";
-$result = $conn->query($sql);
+$requete_sql = "SELECT Timestamp, Valeur_Mesure FROM Donnee_Mesurer";
+$resultat = $connexion->query($requete_sql);
 
-if ($result->num_rows > 0) {
-    header('Content-Type: text/csv');
+// Si on trouve des informations dans la BDD .
+if ($resultat->num_rows > 0) {
+    header('Content-Type: text/csv'); //Telechargement CSV
     header('Content-Disposition: attachment; filename="donnees.csv"');
 
-    $output = fopen('php://output', 'w');
+    //Prépare un endroit pour écrire les données
+    $fichier_sortie = fopen('php://output', 'w');
 
-    fputcsv($output, array('Timestamp', 'Valeur_Mesure'));
+    // Première ligne du fichier CSV noms des colonnes : 'Timestamp' et 'Valeur_Mesure'
+    fputcsv($fichier_sortie, array('Timestamp', 'Valeur_Mesure'));
 
-    while ($row = $result->fetch_assoc()) {
-        fputcsv($output, array($row['Timestamp'], $row['Valeur_Mesure']));
+    // Pour chaque ligne dans la base de données qu'on trouve
+    while ($ligne = $resultat->fetch_assoc()) {
+        //'Timestamp' et 'Valeur_Mesure' sont écrits dans le fichier CSV
+        fputcsv($fichier_sortie, array($ligne['Timestamp'], $ligne['Valeur_Mesure']));
     }
 
-    fclose($output);
-} else {
-    echo "Aucune donnée à exporter.";
+    fclose($fichier_sortie);
 }
-$conn->close();
+$connexion->close();
 ?>
